c4f74adb8eba4c9023f9d9e3ef5df0b3200a1ec1
Maarten van weerdt <34571@ma-web.nl>
2023-02-24 09:03:32 +0100

    flex opdrachten tot variabelen.

---
+++ flex opdracht 1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace flex_opdracht_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}
