6184b314a039a310d3dba827e74266758ac3f2f6
Maarten van weerdt <34571@ma-web.nl>
2023-04-11 22:16:30 +0100

    extra hello world opgave (text in groen)

---
+++ extra world opdracht/extra world opdracht/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace extra_world_opdracht
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Hello, World!";
+            
+            foreach (char letter in text)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(letter);
+                Thread.Sleep(100);
+            }
+            Console.ReadKey();
+        }
+    }
+}
