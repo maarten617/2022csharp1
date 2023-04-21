1fcd0b27d3ac54a7e918d6afa59ed505a6a0d32b
Maarten van weerdt <34571@ma-web.nl>
2023-04-21 09:11:39 +0100

    classes opgaven

---
+++ classes/classes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace classes
+{
+
+    internal class Program
+    {
+        static void Main()
+        {
+           rectangle joe = new rectangle();
+            joe.x = 100;
+            joe.y = 200;
+            joe.width= 50;
+            joe.height= 50;
+        }
+    }
+    public class rectangle
+    {
+        public int x;
+        public int y;
+        public int width;
+        public int height;
+    }
+
+}
